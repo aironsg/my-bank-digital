64dc5970ab3ebd2347b5d026d7a64dd0a12a1051
Airon silva <airon_gm@hotmail.com>
2023-11-30 17:05:22 -0300

    feat: criação layout tela splash

---
+++ app/src/main/java/br/com/devairon/mybankdigital/presenter/splash/SplashFragment.kt
@@ -0,0 +1,23 @@
+package br.com.devairon.mybankdigital.presenter.splash
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.devairon.mybankdigital.R
+import br.com.devairon.mybankdigital.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+
+    private var _binding: FragmentSplashBinding? =  null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+}
